413556e0db43977a9ccd5c40d0d6cf9bd28dad38
hscomp2002 <hscomp2002@yahoo.com>
2015-05-20 01:13:12 +0430

    میرسمیع : اصلاح گند گذشته

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,12 +17,14 @@
      <link rel="stylesheet" href="<?php echo asset_url(); ?>css/bootstrap-select.min.css" type="text/css" />
      <link rel="stylesheet" href="<?php echo asset_url(); ?>css/select2.css" type="text/css" />
      <link rel="stylesheet" href="<?php echo asset_url(); ?>css/xgrid.min.css" type="text/css" />
+     <link rel="stylesheet" href="<?php echo asset_url(); ?>css/fileinput.min.css" type="text/css" />
      <!--<link rel="stylesheet" href="<?php echo asset_url(); ?>css/bootstrap.min.css" />-->
      <link rel="stylesheet" href="<?php echo asset_url(); ?>css/bootstrap-datepicker.min.css" />
      <script src=" <?php echo asset_url().'js/jquery-1.11.1.min.js' ?>" ></script>
      <script src=" <?php echo asset_url().'js/grid.min.js' ?>" ></script>
      <script src=" <?php echo asset_url().'js/bootstrap-datepicker.min.js' ?>"></script>
      <script src=" <?php echo asset_url().'js/bootstrap-datepicker.fa.min.js' ?>"></script>
+     <script src=" <?php echo asset_url().'js/fileinput.min.js' ?>"></script>
     <?php
         if(isset($has_ckeditor) && $has_ckeditor) // if in edit_content ckedotr is activeted
         {    
